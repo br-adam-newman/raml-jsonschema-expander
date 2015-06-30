edff34374107b3ecac41dadfba432f10423bdd64
Adam Newman <adam.newman@bottlerocketapps.com>
2015-06-30 16:15:38 -0500

    Finish expanding schemas

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 var urllibSync = require('urllib-sync');
 
-var schemaCache = {};
+var schemaHttpCache = {};
+var expandedSchemaCache = {};
 
 function expandJsonSchemas(ramlObj) {
     for (var schemaIndex in ramlObj.schemas) {
@@ -11,15 +12,55 @@ function expandJsonSchemas(ramlObj) {
         var schemaText = expandSchema(schema[objectKey]);
         schema[objectKey] = schemaText;
     }
+    
+    for (var resourceIndex in ramlObj.resources) {
+        var resource = ramlObj.resources[resourceIndex];
+        ramlObj.resources[resourceIndex] = fixSchemaNodes(resource);
+    }
+    
+    return ramlObj;
+}
+
+/**
+ *  Walk through the hierarchy provided and replace schema nodes with expanded schema.
+ */
+function fixSchemaNodes(node) {
+    var keys = Object.keys(node);
+    for (var keyIndex in keys) {
+        var key = keys[keyIndex];
+        var value = node[key];
+        if (key === "schema") {
+            var schemaObj = JSON.parse(value);
+            if (schemaObj.id && schemaObj.id in expandedSchemaCache) {
+                node[key] = JSON.stringify(expandedSchemaCache[schemaObj.id], null, 2);
+            }
+        } else if (isObject(value)) {
+            node[key] = fixSchemaNodes(value);
+        } else if (isArray(value)) {
+            node[key] = fixSchemaNodesInArray(value);
+        }
+    }
+    return node;
+}
+
+function fixSchemaNodesInArray(value) {
+    for (var i in value) {
+        var element = value[i];
+        if (isObject(element)) {
+            value[i] = fixSchemaNodes(element);
+        }
+    }
+    return value;
 }
 
 function expandSchema(schemaText) {
-    console.log("Expanding:" + schemaText);
     if (schemaText.indexOf("$ref") > 0) {
         var schemaObject = JSON.parse(schemaText);
         if (schemaObject.id) {
             var basePath = getBasePath(schemaObject.id);
-            return JSON.stringify(walkTree(basePath, schemaObject), null, 2);
+            var expandedSchema = walkTree(basePath, schemaObject);
+            expandedSchemaCache[schemaObject.id] = expandedSchema;
+            return JSON.stringify(expandedSchema, null, 2);
         } else {
             return schemaText;
         }
@@ -34,35 +75,54 @@ function expandSchema(schemaText) {
  */
 function walkTree(basePath, node) {
     var keys = Object.keys(node);
+    var expandedRef;
     for (var keyIndex in keys) {
         var key = keys[keyIndex];
         var value = node[key];
         if (key === "$ref") {
-            node[key] = expandRef(basePath, value);
+            //Node has a ref, create expanded ref in its place.
+            expandedRef = expandRef(basePath, value);
+            delete node["$ref"];
         } else if (isObject(value)) {
-            walkTree(basePath, value);
+            node[key] = walkTree(basePath, value);
         } else if (isArray(value)) {
-            walkArray(basePath, value);
+            node[key] = walkArray(basePath, value);
         }
     }    
+    
+    //Merge an expanded ref into the node
+    if (expandedRef != null) {
+        mergeObjects(node, expandedRef);
+    }
+    
     return node;
 }
 
+function mergeObjects(destination, source) {
+    for (var attrname in source) { destination[attrname] = source[attrname]; }
+}
+
 function expandRef(basePath, value) {
-    var refUri = basePath + value;
+    var refUri = basePath + "/" + value;
     var refText = fetchRef(refUri);
-    console.log("Downloaded ref: " + refText);
-    return {"$ref": value};
+    var refObject = JSON.parse(refText);
+    var newBasePath;
+    if (refObject.id) {
+        newBasePath = getBasePath(refObject.id);
+    } else {
+        newBasePath = basePath;
+    }
+    return walkTree(newBasePath, refObject);
 }
 
 function fetchRef(refUri) {
-    if (refUri in schemaCache) {
-        return schemaCache[refUri];
+    if (refUri in schemaHttpCache) {
+        return schemaHttpCache[refUri];
     } else {
         var request = urllibSync.request;
         var response = request(refUri, { timeout: 30000 });            
         if (response.status == 200) {
-            schemaCache[refUri] = response.data;
+            schemaHttpCache[refUri] = response.data;
         }
         return response.data;
     }
@@ -72,9 +132,10 @@ function walkArray(basePath, value) {
     for (var i in value) {
         var element = value[i];
         if (isObject(element)) {
-            walkTree(basePath, value);
+            value[i] = walkTree(basePath, element);
         }
     }
+    return value;
 }
 
 function isObject(value) {
